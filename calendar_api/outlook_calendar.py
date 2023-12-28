22a59285845c44cf96876dd0de816effae9d8c44
Pulkit Kumar <39520468+buddywhitman@users.noreply.github.com>
2023-12-28 22:28:23 +0530

    Create outlook_calendar.py

---
+++ calendar_api/outlook_calendar.py
@@ -0,0 +1,9 @@
+# calendar_api/outlook_calendar.py
+
+# Implement Outlook Calendar functionality here (if available)
+# Include necessary imports and functions for Outlook Calendar API
+# ...
+
+# Example function (replace with actual implementation)
+def create_outlook_calendar_event(event_details):
+    print("Outlook Calendar Event created:", event_details)
